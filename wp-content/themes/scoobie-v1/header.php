babe706bb9fe9742ff7c8d75f2265ffceace8e18
Matej Marić <maric.matej@hotmail.com>
2020-03-28 17:38:33 +0100

    Bootstrap

--- wp-content/themes/scoobie-v1/header.php
+++ wp-content/themes/scoobie-v1/header.php
@@ -3,11 +3,13 @@
     <head>
         <?php wp_head(); ?>
         <meta charset="utf-8">
-        <title><?php bloginfo('name');?> - <?php wp_title('');?></title>
+        <title><?php wp_title('');?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
         <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap-grid.min.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
+        
     </head>
     <body>
         <header>
